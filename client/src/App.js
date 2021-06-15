cf0b1f47c560ffa098a0d0492f1792a7f42d1903
Uzzwal Gupta <uzzwal.gupta@hoolime.co>
2021-06-15 10:39:29 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React from "react";
+import { Container } from "react-bootstrap";
 
-import './App.scss'
-import Register from './pages/Register'
+import "./App.scss";
+import Register from "./components/Register";
 
 function App() {
   return (
     <Container className="pt-5">
       <Register />
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
